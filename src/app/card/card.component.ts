ccc638a670a8fc0582a467f246fc9da055414f21
emddeveloper <emd.developer@gmail.com>
2021-03-17 10:18:31 +0530

    ngfor, input and all done

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input() post = {
+    title: '',
+    username: '',
+    content: '',
+    imageUrl: '',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
