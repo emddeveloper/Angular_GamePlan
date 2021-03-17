e70a5434a295b7c96d8563d237367a0841ce1f3e
emddeveloper <emd.developer@gmail.com>
2021-03-17 10:02:14 +0530

    cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'gameplan';
+  posts = [
+    {
+      title: 'Big Tree',
+      username: '@emd_aminul',
+      content: 'I saw a big tree today.',
+      imageUrl: 'assets/tree.jpeg',
+    },
+    {
+      title: 'Big TGreat Mountain',
+      username: '@Tania',
+      content: 'I saw a big Mountain.',
+      imageUrl: 'assets/mountain.jpeg',
+    },
+    {
+      title: 'Love to Biking',
+      username: '@aminul',
+      content: 'I love to do biking everyday.',
+      imageUrl: 'assets/biking.jpeg',
+    },
+  ];
 }
